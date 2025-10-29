88e281112cafe4ecbffe4a642bfa236e088ad227
Kiran Dhawan <134703701+Key-runnnnnnn@users.noreply.github.com>
2025-10-29 15:35:02 +0530

    Remove unused Box import from Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import { Box, HStack, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { HStack, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
